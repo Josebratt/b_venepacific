3961711cbcfa969accf67e76e850b8d0b971c2e6
Jose <josebratt@gmail.com>
2022-11-21 17:31:20 -0400

    update categories endpoints

--- src/app/routes/categories.js
+++ src/app/routes/categories.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const {
-  getCategories
+  getCategories,
+  getCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
 } = require("../controllers/categories");
 
 /** get all categories */
 router.get("/", getCategories);
 
+/** getById Category */
+router.get("/:id", getCategory);
+
+/** create category */
+router.post("/", createCategory);
+
+/** update category */
+router.put("/:id", updateCategory);
+
+/** delete category */
+router.delete("/:id", deleteCategory);
 
 module.exports = router;
