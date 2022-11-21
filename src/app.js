0b70abd7224760b8713b14387bfd2c97451e4b72
Jose <josebratt@gmail.com>
2022-11-21 16:37:55 -0400

    define basic route

--- src/app.js
+++ src/app.js
@@ -11,6 +11,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+/** define routes */
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+
 dbConnect();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
