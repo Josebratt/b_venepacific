73cc81e19a2f477491a0d653064b954106b99bb0
Jose <josebratt@gmail.com>
2022-11-21 17:38:03 -0400

    initial product model

---
+++ src/app/models/products.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+  {
+    sku: {
+      type: String,
+      // required: false,
+    },
+    name: {
+      type: String,
+      // required: false,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    cloudinary_id: {
+      type: String,
+    }
+    // category: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "categories",
+    //   // required: false,
+    // },
+    // countInStock: {
+    //   type: Number,
+    //   // required: false,
+    //   min: 0,
+    //   max: 255,
+    // },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+productSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+productSchema.set("toJSON", { virtuals: true });
+
+module.exports = mongoose.model("products", productSchema);
