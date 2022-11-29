42769682230ee6ed35bd31a9565b60cecd358c81
Jose <josebratt@gmail.com>
2022-11-29 16:15:32 -0400

    update user available

--- src/app/utils/jwt.js
+++ src/app/utils/jwt.js
@@ -11,7 +11,7 @@ function authJwt() {
         path: [
             { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
             // { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/users(.*)/, methods: ['GET', 'POST', 'OPTIONS'] },
+            { url: /\/api\/v1\/users(.*)/, methods: ['GET', 'POST', 'PUT', 'OPTIONS'] },
             `${api}/users/login`,
             `${api}/users/register`,
         ]
