959b2a68cb28a6832b95a1e6ff73fe1bb79926bd
Jose <josebratt@gmail.com>
2022-12-06 11:11:50 -0400

    update jwt

--- src/app/utils/jwt.js
+++ src/app/utils/jwt.js
@@ -1,21 +1,27 @@
 const { expressjwt: expressJwt } = require("express-jwt");
 
-function authJwt() { 
+function authJwt() {
     const secret = process.env.SECRET;
     const api = process.env.API_URL;
     return expressJwt({
         secret,
-        algorithms: ['HS256'],
+        algorithms: ["HS256"],
         isRevoked: isRevokedCallback,
     }).unless({
         path: [
-            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
-            // { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'POST', 'PUT','DELETE', 'OPTIONS'] },
-            { url: /\/api\/v1\/users(.*)/, methods: ['GET', 'POST', 'PUT', 'OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+            {
+                url: /\/api\/v1\/orders(.*)/,
+                methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            },
+            {
+                url: /\/api\/v1\/users(.*)/,
+                methods: ["GET", "POST", "PUT", "OPTIONS"],
+            },
             `${api}/users/login`,
             `${api}/users/register`,
-        ]
+        ],
     });
 }
 
@@ -24,6 +30,6 @@ const isRevokedCallback = async (req, token) => {
     const tokenId = token.payload.jti;
     const tokenUser = await data.getRevokedToken(isAdmin, tokenId);
     return tokenUser !== "undefined";
-  };
+};
 
 module.exports = authJwt;
